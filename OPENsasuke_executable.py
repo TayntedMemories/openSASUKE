b7fd6e6f17269db95db6a1422d1fc676ec190d9c
Cain Baidh <celontinoli@gmail.com>
2026-07-11 04:35:20 -0400

    first update

--- OPENsasuke_executable.py
+++ OPENsasuke_executable.py
@@ -1,60 +1,125 @@
 def pass_fail_Calculation(): # This is the core/heart of the simulation, so to speak.
     
-    def setup():
+    def loaddata():
+        
         ob = input("ENTER OBSTACLE FILE:")
+        
         with open(ob, 'r') as obstacle:
+            global obstacle_data
+            
             obstacle_data = yaml.full_load(obstacle)
+            
+            # debug code start
             print(obstacle_data)
-            global obstat
-            obstat = next(iter(obstacle_data.values()))
-            print(obstat)
+            
+            print(type(obstacle_data))
+            # debug code end
+                    
         comp = input("ENTER COMPETITOR FILE:")
+        
         with open(comp, 'r') as competitor:
+            
+            global competitor_data
+            
             competitor_data = yaml.full_load(competitor)
+            
             print(competitor_data)
-            global compstat
-            compstat = next(iter(competitor_data.values()))
-    
+
+    def fuck():
+        
+        global obstat
+        
+        obstat = set(obstacle_data.keys()) if isinstance(obstacle_data, dict) else set()
+        
+        print(obstat)
+        
+        global compstat
+        
+        compstat = set(competitor_data.keys()) if isinstance(competitor_data, dict) else set()  
+        
+        print(compstat)
+
+        global common
+        
+        common = obstat & compstat
+        
+        print(f"COMMON:", common)
+        
+    def shit():
+        finval1 = None
+        finval2 = None
+        for val in common:
+            x = val
+            y = val
+            val1 = competitor_data[x]
+            
+            val2 = obstacle_data[y]
+
+
+            print(val1, val2)
+
+
+
     def adversary_set():
+        
         chaos_Var = random.uniform(1.5, 1.7)
+       
         global pass_Probability
+        
         pass_Probability = round(((compstat / chaos_Var) / obstat), 2)
+        
         print(f"CHAOS HAS BEEN SET TO {chaos_Var}")
+        
         print(f"WILL THEY PASS; {pass_Probability}")
     
     def execute_Run():
+        
         against_Competitor = round(random.uniform(0.1, 1.3), 2)
+        
         print(f"WILL THEY FAIL; {against_Competitor}")
+        
         print(f"{against_Competitor} > {pass_Probability}")
+        
         if against_Competitor > pass_Probability:
+            
             print("COMPETITOR HAS FAILED.")
+        
         else:
+            
             print("COMPETITOR HAS PASSED")
     
-    setup()
+    loaddata()
+
+    fuck()
+
+    shit()
+    
     adversary_set()
+    
     execute_Run()
-
-def yaml_Testing():
-    file = input()
-    with open(file, 'r') as parse:
-        data = yaml.full_load(parse)
-        print(data)
-
 # UI code goes here.
 import yaml
+
 import random
 
 print("Welcome to OPENsasuke. // VERSION: EARLYDEVELOPMENT // By Cain // Copyright is for wusses")
+
 print("""Type in '1' to test the pass/fail function. Type in '2' to test YAML loading. | Type in 'Is This A Ninja Machine?' for a funny.""")
+
 print("What do you want to do?")
+
 selection = input()
+
 if selection == "1":
+    
     print("ENTERING PASS/FAIL DEBUG")
+    
     pass_fail_Calculation()
+
 elif selection == "Is This A Ninja Machine?":
+    
     print("*A* ninja machine, but not THE Ninja Machine.")
-elif selection == "2":
-    yaml_Testing()
+
 else:
+    
     print("INVALID, BAILING OUT!")
